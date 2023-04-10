239f024253f73051d2bc316300fb06bb1f237614
Luiz Henrique Oliveira de Assis <51269270+luizhenrique055@users.noreply.github.com>
2023-04-10 17:07:51 -0300

    Att

---
+++ LetCode/LetCode1.java
@@ -0,0 +1,14 @@
+public class LetCode1 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+  
+        }
+    }
+
+}
